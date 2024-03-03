73160e5932bc42fd295ee740aad4ad3d685cff5c
ramesh_b <86396489+rameshboggu36@users.noreply.github.com>
2024-03-03 21:05:22 +0530

    Updated app.py with few changes

--- app.py
+++ app.py
@@ -21,31 +21,33 @@ def main():
     # loading the cam model and MNIST_model 
     model, cam = load_model()
     
-    st.header("Space to Visualize the GradCam for a Number")
+    st.header("Hand written Digit recognizer along with Heatmap by Ramesh")
     st.divider()
-    uploaded_file = st.file_uploader("Upload the image to classify it and also to visulaize it's Grad Cam")
+    uploaded_file = st.file_uploader("Upload the image to classify and also to visulaize it's heatmap")
 
     if uploaded_file is not None:
         # Reading the uploaded image into pil and then into torch.tensor format
-        pil_img = ImageOps.grayscale(Image.open(uploaded_file).resize((28,28)))
-        torch_tensor = transforms.ToTensor()(pil_img).view(-1,1,*pil_img.size)
+        pil_img = ImageOps.grayscale(Image.open(uploaded_file).resize((28, 28)))
+        torch_tensor = transforms.ToTensor()(pil_img).view(-1, 1, *pil_img.size)
 
         # generating the heat map using cam model
         gray_scale_cam = cam(input_tensor=torch_tensor)
         prediction = model(torch_tensor)
-
-
-        in_img_vis = torch_tensor.permute(0,2,3,1).numpy() # converting this into B, W, H, C 
-        #this line combines the original image and the heat map generated using the cam model
-        visualization = show_cam_on_image(in_img_vis, np.transpose(gray_scale_cam,(1,2,0)),image_weight=0.7)  
+        
+        in_img_vis = torch_tensor.permute(0, 2, 3, 1).numpy()  
+        st.image(in_img_vis, width=480, caption='Uploaded Image')
+        # this line combines the original image and the heat map generated using the cam model
+        visualization = show_cam_on_image(in_img_vis, np.transpose(gray_scale_cam, (1, 2, 0)), image_weight=0.7)
+        # st.write(visualization)
         conf = torch.max(prediction).item()
         pred = torch.argmax(prediction).item()
-        st.toast(f'The confidence score and the prediction is: {conf}, {pred}')
-        if conf >=0.5:
-            st.image(Image.fromarray(visualization.squeeze()).resize((480,480)))
+        st.write(f'The confidence score and the prediction is: {conf}, {pred}')
+        if conf >= 0.5:
+            st.image(Image.fromarray(visualization.squeeze()).resize((480, 480)),caption='Generated Image with Heatmap')
             st.success(f"The model has predicted this to be a {pred}", icon="✅")
         else:
-            st.warning(f'Upload the image containing the number', icon="⚠️")
+            st.warning(f'Upload the correct image containing the number', icon="⚠️")
+
         
 if __name__=="__main__":
     main()
